9d9001ab8fea3668f57aae6b8f8949bbf926d2c7
LamiaUddin <lamiahameed@yahoo.com>
2021-01-11 21:43:31 -0500

    Updates

---
+++ precache-manifest.76483b542721089b0fb3a75d48f3f2b8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97d94e9ae6ceb185faa83392f93f0bb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b86dd0e8feaa0c7e5de5",
+    "url": "/static/css/2.234e5024.chunk.css"
+  },
+  {
+    "revision": "14c4b8e07540db9d0a5e",
+    "url": "/static/css/main.ea73098a.chunk.css"
+  },
+  {
+    "revision": "b86dd0e8feaa0c7e5de5",
+    "url": "/static/js/2.0f5d72cc.chunk.js"
+  },
+  {
+    "revision": "2c21478778d46668a7c75a32a167f767",
+    "url": "/static/js/2.0f5d72cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14c4b8e07540db9d0a5e",
+    "url": "/static/js/main.f928e084.chunk.js"
+  },
+  {
+    "revision": "8d0a98eb7fd2f4ecd6b6",
+    "url": "/static/js/runtime-main.d4bdd09a.js"
+  },
+  {
+    "revision": "5c069ae803cd1696a30f655646aa0801",
+    "url": "/static/media/acv.5c069ae8.jpeg"
+  },
+  {
+    "revision": "02bc693595afe4aed3a23144a5cde167",
+    "url": "/static/media/amst.02bc6935.JPG"
+  },
+  {
+    "revision": "525f5dec5602372bdc368718e32e6fd5",
+    "url": "/static/media/aust.525f5dec.jpeg"
+  },
+  {
+    "revision": "f278be7973516129af9e64d45a0c6f4d",
+    "url": "/static/media/c++.f278be79.png"
+  },
+  {
+    "revision": "de83b3acae97cfb5cc771ec5f9d710d5",
+    "url": "/static/media/csharp.de83b3ac.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "19bdf9acb664d48b7661fe7da7258523",
+    "url": "/static/media/new.19bdf9ac.jpeg"
+  },
+  {
+    "revision": "f3c9732b256032731b522aa9bc0c25dd",
+    "url": "/static/media/pro.f3c9732b.png"
+  },
+  {
+    "revision": "876a8325687358ce46ec26be29118535",
+    "url": "/static/media/react.876a8325.svg"
+  },
+  {
+    "revision": "99c4dfa721cf9258ca0355e31936063d",
+    "url": "/static/media/tech.99c4dfa7.png"
+  },
+  {
+    "revision": "b7e9f037a3b24046d1714dad6b880ba9",
+    "url": "/static/media/travel.b7e9f037.jpg"
+  }
+]);
